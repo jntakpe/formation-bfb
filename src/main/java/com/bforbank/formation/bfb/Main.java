4e060a326ec478a9644a3a723be9cd005dfc9f9f
jntakpe <j.ntakpe@gmail.com>
2014-08-29 23:11:58 +0200

    EX6 - Jodatime

--- src/main/java/com/bforbank/formation/bfb/Main.java
+++ src/main/java/com/bforbank/formation/bfb/Main.java
@@ -1,6 +1,7 @@
 package com.bforbank.formation.bfb;
 
-import org.apache.commons.lang3.StringUtils;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Classe mère de l'application
@@ -10,8 +11,26 @@ import org.apache.commons.lang3.StringUtils;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String bonjour = "bonjour";
-        StringUtils.reverse(bonjour);
+        instant();
+        period();
     }
 
+
+    public static void instant() {
+        Instant now = Instant.now();
+        for (int i = 0; i < 10000; i++) {
+            String toto = "toto" + i;
+        }
+        Instant end = Instant.now();
+        Duration elapsed = Duration.between(now, end);
+        System.out.println(elapsed.toMillis());
+        elapsed = elapsed.plus(2L, ChronoUnit.SECONDS); //Mutable or immutable ?
+        System.out.println(elapsed.toMillis());
+    }
+
+    public static void period() {
+        LocalDate now = LocalDate.now();
+        LocalDate lemonStart = LocalDate.of(2014, Month.MAY, 11);
+        System.out.println("Jours depuis le début de Lemon : " + lemonStart.until(now, ChronoUnit.DAYS));
+    }
 }
