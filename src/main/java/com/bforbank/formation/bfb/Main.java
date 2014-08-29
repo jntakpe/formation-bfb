92c2684cb5bb0365f4fd2432e79b1046058ce736
jntakpe <j.ntakpe@gmail.com>
2014-08-29 22:21:23 +0200

    Init

---
+++ src/main/java/com/bforbank/formation/bfb/Main.java
@@ -0,0 +1,14 @@
+package com.bforbank.formation.bfb;
+
+/**
+ * Classe mère de l'application
+ *
+ * @author jntakpe
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
