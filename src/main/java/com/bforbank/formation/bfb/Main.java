d0d09d057d2b8819423b91db5fa912d33294b915
jntakpe <j.ntakpe@gmail.com>
2014-08-29 22:28:24 +0200

    EX2 - immutable

--- src/main/java/com/bforbank/formation/bfb/Main.java
+++ src/main/java/com/bforbank/formation/bfb/Main.java
@@ -12,33 +12,16 @@ public class Main {
     public static void main(String[] args) {
     }
 
-
-    private static void primTest() {
-        int a = 1;
-        int b = 1;
-        System.out.println(a == b);
+    private static void strReplace() {
+        String a = "toto";
+        a.toUpperCase();
+        System.out.println(a);
     }
 
-    private static void objTest() {
-        long sysDate = System.currentTimeMillis();
-        Date a = new Date(sysDate);
-        Date b = new Date(sysDate);
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
-    }
-
-    private static void strTest() {
-        String a = "test";
-        String b = "test";
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
-    }
-
-    private static void strNewTest() {
-        String a = new String("test");
-        String b = new String("test");
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
+    private static void dateReplace() {
+        Date date = new Date();
+        date.setTime(0);
+        System.out.println(date.getTime());
     }
 
 }
